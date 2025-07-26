6ea6a7862d87ba19d018f1b8768c6ac43a000193
GeniusNirwal <khushalnirwal@gmail.com>
2025-07-26 11:30:50 +0530

    SMA stratergy added

--- data_handler.py
+++ data_handler.py
@@ -6,4 +6,12 @@ class DataLoader:
         self.start_date = start
         self.end_date = end
     def fetch_data(self):
-        return yf.download(self.ticker, start=self.start_date, end=self.end_date)
+        return yf.download(self.ticker, start=self.start_date, end=self.end_date, auto_adjust=True)
+    
+if __name__ == "__main__":
+    ticker = input("Enter the ticker symbol (.NS at end if indian stock)").upper()
+    start_date = input("Enter the start date(YYYY-MM-DD): ")
+    end_date = input("Enter the end date(YYYY-MM-DD): ")
+    dataload = DataLoader(ticker, start_date,end_date)
+    data = dataload.fetch_data()
+    # print(data['Close'])
---
+++ stratergies.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+class stratergy:
+    def generate_signals(self, data):
+        raise NotImplemented
+
+# SIMPLE MOVING AVERAGE STRATERGY
+class sma(stratergy):
+    def __init__(self, sma_length, lma_length):
+        self.short_moving_average=sma_length
+        self.long_moving_average = lma_length
+
+    def calculate_smas(self,data):
+        signals = pd.DataFrame(index=data.index)
+        closing_price = data['Close']
+        signals['price'] = closing_price
+        signals['sma'] = closing_price.rolling(window=self.short_moving_average, min_period=1).mean()
+        signals['lma'] = closing_price.rolling(window=self.long_moving_average, min_period=1).mean()
+        return signals
+
+    def generate_signals(self,data):
+        signals = self.calculate_smas()
+        #Buy Signal
+        signals['signal'][20:] = np.where((signals['sma'][20:] > signals['lma'][20:]) & (signals['sma'].shift(1)[20:] <= signals['lma'].shift(1)[20:]),1.0, 0.0)
+        #Sell Signal
+        signals['signal'][20:] = np.where((signals['sma'][20:] < signals['lma'][20:]) & (signals['sma'].shift(1)[20:]>= signals['lma'].shift(1)[20:]), -1.0, signals['signal'][20:])
+        signals['positions'] = signals['signals'].diff()
+        return signals
+
